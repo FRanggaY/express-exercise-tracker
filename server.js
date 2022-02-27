e3653c292c882ea4191c08534e7ced5a5020537d
FRanggaY <franciscusrangga@gmail.com>
2022-02-27 21:20:33 +0700

    update server

--- server.js
+++ server.js
@@ -2,17 +2,24 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const connectDB = require('./db/connect')
+const user = require('./routes/user');
+const bodyParser = require('body-parser');
 
+const port = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
 app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.use('/api', user)
 
+connectDB
 
-
-
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+app.listen(port, function () {
+  console.log('Listening to ' + port)
+});
